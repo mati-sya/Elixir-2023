9fb96e54d845f84ddc66f897baeb5deb08fb8127
mati-sya <matildaschauf@yahoo.de>
2023-07-19 10:16:05 +0900

    changeset, validation

--- day7/example/priv/repo/migrations/20230717064556_create_users.exs
+++ day7/example/priv/repo/migrations/20230717064556_create_users.exs
@@ -6,9 +6,13 @@ defmodule Example.Repo.Migrations.CreateUsers do
       add :first_name, :string, null: false
       add :last_name, :string, null: false
       add :age, :integer
+      add :email, :string, null: false
 
       # inserted_at, updated_at colums
       timestamps()
     end
+
+    # set email as unique index
+    create unique_index(:users, [:email])
   end
 end
