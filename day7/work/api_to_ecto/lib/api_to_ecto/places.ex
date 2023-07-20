1a1dc55650a0ebefce69fc85c9bdb939552a28a6
mati-sya <matildaschauf@yahoo.de>
2023-07-20 13:57:29 +0900

    Ecto.Queryとカスタムバリデーションを使用したワーク

--- day7/work/api_to_ecto/lib/api_to_ecto/places.ex
+++ day7/work/api_to_ecto/lib/api_to_ecto/places.ex
@@ -1,6 +1,7 @@
 defmodule ApiToEcto.Places do
   alias ApiToEcto.Repo
   alias ApiToEcto.Place
+  import Ecto.Query
 
   # すべてのデータを取得する関数の作成
   def get_places() do
@@ -41,4 +42,32 @@ defmodule ApiToEcto.Places do
   end
 
   def delete_place(_id), do: nil
+
+  # クエリを使用して絞り込んでから取得する
+  # e.g. name 131200002001
+  def get_place_by_name(name \\ "131200002001") do
+    query = from(p in Place, where: p.name == ^name)
+    # クエリから1つのデータを取得する関数
+    Repo.one(query)
+  end
+
+  # クエリのセレクトを使用して特定のカラムの値のみを取得する
+  def select_address() do
+    query = from(p in Place, select: p.address)
+    Repo.all(query)
+  end
+
+  # アドレス検索
+  def get_places_to_search_by_address(address) do
+    # SQL pattern
+    pattern = "%#{address}%"
+    query = from(p in Place, where: like(p.address, ^pattern))
+    Repo.all(query)
+  end
+
+  def ad_search() do
+    get_places_to_search_by_address("石神井台")
+  end
+
+  # ---
 end
